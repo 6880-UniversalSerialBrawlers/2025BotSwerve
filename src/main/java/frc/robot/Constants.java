5dc267c9ef688d8ceec8d55e6698353941d95c95
Siddharth <supernebula31@gmail.com>
2025-03-05 16:30:33 -0500

    can ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,8 +21,8 @@ public final class Constants {
 
   public static final class CoralSubsystemConstants {
     public static final int kElevatorMotorCanId = 51;
-    public static final int kArmMotorCanId = 3;
-    public static final int kIntakeMotorCanId = 2;
+    public static final int kArmMotorCanId = 61;
+    public static final int kIntakeMotorCanId = 62;
 
     public static final class ElevatorSetpoints {
       public static final int kFeederStation = 0;
